1fd7c6ac64a0ce8cda5247cbec1536a630717741
Geetansh Saxena <saxenageetansh1@gmail.com>
2026-04-28 15:50:06 +0530

    feat: enable scheduled methods

--- src/main/java/com/payu/mdr/MdrApplication.java
+++ src/main/java/com/payu/mdr/MdrApplication.java
@@ -2,12 +2,13 @@ package com.payu.mdr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MdrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MdrApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MdrApplication.class, args);
+    }
 }
